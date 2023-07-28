1a5c45cccffb327d6fed58dd8b024c15e8907410
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-07-28 06:27:20 -0700

    multer-storage issue fixed

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,17 @@
 import multer from "multer";
+import os from 'os'
+import path from "path";
 
+// const upload = multer({dest:"uploads"})
 
-const upload = multer({dest:"uploads"})
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, os.tmpdir()); // Use the /tmp directory for temporary storage
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
 
-
+const upload = multer({ storage: storage });
 export default upload
